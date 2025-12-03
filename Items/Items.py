233bd8db91392da899b05e704e5e3bc8048cf930
Alexander Abbosh <alex.abbosh@gmail.com>
2025-12-04 00:27:11 +1000

    Finished weapon and item type generation methods

--- Items/Items.py
+++ Items/Items.py
@@ -2,17 +2,32 @@
 import random
 
 class Item:
-    def __init__(self, name, type):
-        self.name = name
-        self.type = type
+    def __init__(self, player_level):
+        self.player_level = player_level
+        self.type = self.generate_type()
 
     def generate_type(self):
         types = ['Weapon', 'Armor', 'Potion', 'Spell']
-
+        rand_index = types[random.randint(0, len(types))]
+        return types[rand_index]
 
 class Weapon(Item):
-    def generate_weapon(self, player_level):
-        low_level_weapons = {'Wood Sword', 'Wood Staff'}
-        med_level_weapons = low_level_weapons + {'Iron Sword', 'Iron Staff'}
+    def __init__(self, name, damage):
+        weapon_name, weapon_damage = self.generate_weapon()
+        self.name = weapon_name
+        self.damage = weapon_damage
+
+    def generate_weapon(self):
+        low_level_weapons = {'Wood Sword': 2, 'Wood Staff': 2} # Dictionary which stores weapon name (key) and damage (value)
+        med_level_weapons = low_level_weapons + {{'Iron Sword': 4}, {'Iron Staff': 4}}
         high_level_weapons = med_level_weapons + {'Steel Sword', 'Steef Staff'}
-        
+        random_index = None
+        if Item.player_level <= 5:
+            random_index = random.randint(0, len(low_level_weapons)) # Generate random index
+            return low_level_weapons[random_index]
+        elif Item.player_level > 5 and Item.player_level < 10:
+            random_index = random.randint(0, len(med_level_weapons))
+            return med_level_weapons[random_index]
+        else:
+            random_index = random.randint(0, len(high_level_weapons)) 
+            return high_level_weapons[random_index]
