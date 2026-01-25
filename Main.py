038222f5cf4852ab8f05ea6f2e513a1aed34df85
Alexander Abbosh <alex.abbosh@gmail.com>
2026-01-26 08:54:43 +1000

    Modified character attributes class

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-from Character.Character_Setup import *
+from Character.Character_Attributes import *
 from Gameplay.Encounter_Generation import *
 from Character.Inventory import *
 
@@ -20,22 +20,14 @@ def main():
     
     # Creating character
     name = input("Enter your character's name: ")
-    print("- Race Selection -")
-    print("Pick your race from the following:")
-    print("- HUMAN (Press h): Medium Strength, Medium Magic, Medium Resilience, Medium Speed, Medium Intelligence")
-    print("- ELF (Press e): Low Strength, High Magic, Low Resilience, Medium Speed, High Intelligence")
-    print("- ORC (Press o): High Strength, Low Magic, High Resilience, Low Speed, Medium Intelligence")
-    race = input("Enter h (human), e (elf), or o (orc): ")
-    while race not in "heo":
-        race = input("Enter either h, e, or o: ")
-
-    # Saving info to character class
-    if race == "h":
-        Character(name, race, 20, 20, 20, 20, 20, 20, 0)
-    elif race == "e":
-        Character(name, race, 10, 30, 10, 20, 30, 20, 0)
-    else:
-        Character(name, race, 30, 10, 30, 10, 20, 20, 0)
+    print("- Class Selection -")
+    print("Pick your class from the following:")
+    print("- Warrior (Press h): Medium Strength, Medium Magic, Medium Resilience, Medium Speed, Medium Intelligence")
+    print("- Mage (Press e): Low Strength, High Magic, Low Resilience, Medium Speed, High Intelligence")
+    print("- Tank (Press o): High Strength, Low Magic, High Resilience, Low Speed, Medium Intelligence")
+    player_class = input("Enter h (human), e (elf), or o (orc): ")
+    while player_class not in "heo":
+        player_class = input("Enter either h, e, or o: ")
 
     print(f"\nWelcome, {name}!\n")
 
